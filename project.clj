efd6dc25cb8529fbe1c63782150e30f0d1f49a29
Andrei Ursan <hello@andreiursan.com>
2015-12-23 15:35:02 +0200

    updated the license and the project url

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ubb-api "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/victorursan/ubb-api"
+  :license {:name "The BSD 2-Clause License"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
